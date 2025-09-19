4122da9b2d4f73b54fa05522ed9a18f940297aeb
Supriyo-Brandstory <supriyo@brandstory.in>
2025-09-19 19:26:08 +0530

    ..

--- src/component/pages/common/CommonAdvertise.jsx
+++ src/component/pages/common/CommonAdvertise.jsx
@@ -7,15 +7,15 @@ const CommonAdvertise = ({btnOneLink, btnTwoLink, btn1, btn2, title1, title2, de
         <div className={styles.frame}>
             <div className={styles.innerFrame}>
                 <span>
-                    <h1 className={styles.heading}>{title1}</h1>
-                    {title2 && <h1 className={styles.heading}>{title2}</h1>}
+                    <h2 className={styles.heading}>{title1}</h2>
+                    {title2 && <p>{title2}</p>}
                 </span>
                 {description && <p>{description}</p>}
                 <div class="flex gap-x-4">
                   <Link href={btnOneLink} class="orange-btn">
                     {btn1}
                   </Link>
-                  <Link href={btnTwoLink} class="orange-btn">
+                  <Link href={btnTwoLink} class="orange-btn-transparent">
                     {btn2}
                   </Link>
                 </div>
