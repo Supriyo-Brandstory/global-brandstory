68f015de7e89fe417010a1e9d9b68f0494798999
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-01-14 16:46:19 +0530

    ...

--- src/component/pages/common/CommonBanner.jsx
+++ src/component/pages/common/CommonBanner.jsx
@@ -1,34 +1,51 @@
-import React from 'react'
-import styles from '@/style/common/commonBanner.module.css'
+import React from 'react';
+import styles from '@/style/common/commonBanner.module.css';
 
-export const CommonBanner = ({heading, paragraph, paragraph2, buttonLink, buttonLink2, buttonText, buttonText2}) => {
-    return (
-        <div className={styles.seoBanner}>
-            <div className={styles.seoBannerContent}>
-                <h1 className=''><div dangerouslySetInnerHTML={{__html: heading}}></div></h1>
-                <div className={styles.seoBannerParagraph}>
-                    <div dangerouslySetInnerHTML={{__html: paragraph}}></div>
-                    <div dangerouslySetInnerHTML={{__html: paragraph2}}></div>
-                </div>
-                <div className={styles.btnWrapper}>
-                    <div className={styles.seoBannerButton}>
-                        <a href={buttonLink} className={styles.seoBannerButtonLink}>{buttonText}
-                        <img src="/images/triangle-btn.png" alt="btn img" className={styles.btnImg}/>
-                        </a>
-                    </div>
-                    {buttonText2 && 
-                    <div className={styles.seoBannerButton}>
-                        <a href={buttonLink2} className={styles.seoBannerButtonLink2}>{buttonText2}</a>
-                    </div>}
-                </div>
-            </div>
+export const CommonBanner = ({heading,paragraph,paragraph2,buttonLink,buttonLink2,buttonText,buttonText2}) => {
+  const finalLink1 = buttonLink || 'localhost:3000/our-services';
+  const finalLink2 = buttonLink2 || '/contact';
 
-            <div className={styles.boxes}>
-                <div className={styles.box1}></div>
-                <div className={styles.box2}></div>
-                <div className={styles.box3}></div>
-            </div>
+  return (
+    <div className={styles.seoBanner}>
+      <div className={styles.seoBannerContent}>
+        <h1>
+          <div dangerouslySetInnerHTML={{ __html: heading }} />
+        </h1>
+
+        <div className={styles.seoBannerParagraph}>
+          <div dangerouslySetInnerHTML={{ __html: paragraph }} />
+          {paragraph2 && <div dangerouslySetInnerHTML={{ __html: paragraph2 }} />}
         </div>
-    )
-}
- 
+
+        <div className={styles.btnWrapper}>
+          {/* Primary button - always shown */}
+          <div className={styles.seoBannerButton}>
+            <a href={finalLink1} className={styles.seoBannerButtonLink}>
+              {buttonText || 'Learn More'} {/* optional fallback text */}
+              <img
+                src="/images/triangle-btn.png"
+                alt="btn img"
+                className={styles.btnImg}
+              />
+            </a>
+          </div>
+
+          {/* Secondary button - only shown if buttonText2 exists */}
+          {buttonText2 && (
+            <div className={styles.seoBannerButton}>
+              <a href={finalLink2} className={styles.seoBannerButtonLink2}>
+                {buttonText2}
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className={styles.boxes}>
+        <div className={styles.box1} />
+        <div className={styles.box2} />
+        <div className={styles.box3} />
+      </div>
+    </div>
+  );
+};
