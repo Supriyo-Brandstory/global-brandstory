628c80bf8363d06a258390de2b5d004abfb54db7
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-02-10 14:40:25 +0530

    ...

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 
 const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://127.0.0.1:1337';
-const STRAPI_TOKEN = process.env.STRAPI_API_TOKEN;
+const STRAPI_TOKEN = process.env.STRAPI_PROD_API_TOKEN;
 
 export async function fetchStrapi(endpoint, options = {}) {
     const res = await fetch(`${STRAPI_URL}/api/${endpoint}`, {
