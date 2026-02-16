29a3fdac7ffcfde71cce37892376d20dab0f82fb
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-02-16 16:24:17 +0530

    ...

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -27,13 +27,13 @@ export default function RootLayout({ children }) {
       }}
     />
       </head>
+    <body>
       <noscript dangerouslySetInnerHTML={{
         __html:`
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWWSNR99"
       height="0" width="0" style="display:none;visibility:hidden"></iframe>
         `
       }} />
-      <body>
          <Header/>
         {children}
          <Footer/>
