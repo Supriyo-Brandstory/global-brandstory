d68fbb5151c08cdb2d1896b83b5f3711de87e448
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-02-20 18:07:00 +0530

    ...

--- src/component/pages/common/CommonFooterBanner.jsx
+++ src/component/pages/common/CommonFooterBanner.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styles from '@/style/common/commonFooterBanner.module.css';
 
-export const CommonFooterBanner = ({ title, description, btn }) => {
+// We add 'url' to the props and set a default string (e.g., '/')
+export const CommonFooterBanner = ({ 
+  title, 
+  description, 
+  btn, 
+  url = '/contact-us' 
+}) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>{description}</p>
-        <button className={styles.btn}>{btn}</button>
+        
+        {/* Wrapping the button in an anchor tag for redirection */}
+        <a href={url} className={styles.linkWrapper}>
+          <button className={styles.btn}>{btn}</button>
+        </a>
       </div>
     </div>
   );
