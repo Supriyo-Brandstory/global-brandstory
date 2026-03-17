046b23b37f2572a318e3991847a785059404a940
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-03-17 18:24:16 +0530

    ...

--- src/component/pages/common/CommonLabels.jsx
+++ src/component/pages/common/CommonLabels.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
-import styles from '@/style/common/commonLabels.module.css';
+"use client";
+import React from "react";
+import styles from "@/style/common/commonLabels.module.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 export const CommonLabels = ({ title, data }) => {
+  // ✅ Chunk data into groups of 4
+  const chunkedData = [];
+  for (let i = 0; i < data.length; i += 4) {
+    chunkedData.push(data.slice(i, i + 4));
+  }
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.overlay}/>
+      <div className={styles.overlay} />
+
       <div className={styles.container}>
         <h2 className={styles.title}>{title}</h2>
 
+        {/* Desktop Grid */}
         <div className={styles.grid}>
           {data.map((item, index) => (
-            <div key={index} className={styles.label}>{item}</div>
+            <div key={index} className={styles.label}>
+              {item}
+            </div>
           ))}
+        </div>
+
+        {/* Mobile / Slider */}
+        <div className={styles.mobileSlider}>
+          <Swiper
+            modules={[Pagination]}
+            spaceBetween={16}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+          >
+            {chunkedData.map((group, i) => (
+              <SwiperSlide key={i}>
+                <div className={styles.slideGrid}>
+                  {group.map((item, idx) => (
+                    <div key={idx} className={styles.label}>
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </div>
--- src/component/pages/common/CommonWhiteGrid.jsx
+++ src/component/pages/common/CommonWhiteGrid.jsx
@@ -1,32 +1,62 @@
+"use client";
+
 import styles from "@/style/common/commonWhiteGrid.module.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const CommonWhiteGrid = ({ data }) => {
   return (
     <div className={styles.frame}>
       <h2 className={styles.title}>Global Impact - Proven Performance</h2>
 
-      <div className={styles.gridWrapper 
-      }>
-        {
-        data.map((item, index) => (
+      {/* Desktop Grid */}
+      <div className={styles.gridWrapper}>
+        {data.map((item, index) => (
           <div
-
             key={index}
-            className=
-            {`${styles.card} ${item.size === "lg" ? styles.lg : styles.sm}`}
+            className={`${styles.card} ${
+              item.size === "lg" ? styles.lg : styles.sm
+            }`}
           >
-            <div 
-            className={styles.statRow}>
+            <div className={styles.statRow}>
               <h2 className={styles.stat}>{item.stat}</h2>
-              {item.statIcon && <img src={item.statIcon} alt="" className={styles.icon} />}
+              {item.statIcon && (
+                <img src={item.statIcon} alt="" className={styles.icon} />
+              )}
             </div>
-            <h2 
-            className={styles.cardTitle}>{item.title}
-            </h2>
-            <
-              p className={styles.desc}>{item.description}</p>
+            <h2 className={styles.cardTitle}>{item.title}</h2>
+            <p className={styles.desc}>{item.description}</p>
           </div>
         ))}
+      </div>
+
+      {/* Mobile Swiper */}
+      <div className={styles.mobileSlider}>
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={16}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          {data.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className={styles.card}>
+                <div className={styles.statRow}>
+                  <h2 className={styles.stat}>{item.stat}</h2>
+                  {item.statIcon && (
+                    <img src={item.statIcon} alt="" className={styles.icon} />
+                  )}
+                </div>
+                <h2 className={styles.cardTitle}>{item.title}</h2>
+                <p className={styles.desc}>{item.description}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
